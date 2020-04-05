6d529b27df1ec57be0f06d6acae059ee2a411113
CitRuS-A11 <polycyber123@gmail.com>
2020-04-05 12:39:57 +0300

    First Commit

---
+++ app/src/main/java/com/tutorial/learnblender3d/Models/CustomModel.java
@@ -0,0 +1,44 @@
+package com.tutorial.learnblender3d.Models;
+
+import android.graphics.drawable.Drawable;
+
+import com.tutorial.learnblender3d.R;
+
+import java.io.Serializable;
+
+public class CustomModel implements Serializable {
+
+    String title;
+    String description;
+    int imageResource = R.drawable.blender_logo;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    public void setImageResource(int imageResource) {
+        this.imageResource = imageResource;
+    }
+
+    public CustomModel(String title, String description, int imageResource) {
+        this.title = title;
+        this.description = description;
+        this.imageResource = imageResource;
+    }
+}
